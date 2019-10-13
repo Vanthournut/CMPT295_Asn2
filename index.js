230ea62925fdb69891752068509e6507923b9f39
Jarrod Vanthournout <jvanthou@sfu.ca>
2019-10-13 16:35:43 -0700

    server prep

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg')
+var pool
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
